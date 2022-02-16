50261664afe2f9e3f5fb99a73d2c10c765acf6a5
sharjeelyunus <sharjeel924@gmail.com>
2022-02-17 01:39:00 +0500

    👌 IMPROVE: Wallet Card - Welcome section

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,3 +1,9 @@
+import { AiFillPlayCircle } from 'react-icons/ai';
+import { SiEthereum } from 'react-icons/si';
+import { BsInfoCircle } from 'react-icons/bs';
+
+import { Loader } from './';
+
 const commonStyles =
   'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
 
@@ -14,6 +20,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 
 const Welcome = () => {
   const connectWallet = () => {};
+  const handleSubmit = () => {};
 
   return (
     <div className='flex w-full justify-center items-center'>
@@ -41,6 +48,25 @@ const Welcome = () => {
             <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
             <div className={commonStyles}>Low fees</div>
             <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+          </div>
+        </div>
+
+        <div className='flex flex-col flex-1 items-center justify-start w-full md:mt:0 mt-10'>
+          <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'>
+            <div className='flex justify-between flex-col w-full h-full'>
+              <div className='flex justify-between items-start'>
+                <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
+                  <SiEthereum fontSize={21} color='#fff' />
+                </div>
+                <BsInfoCircle fontSize={17} color='#fff' />
+              </div>
+              <div>
+                <p className='text-white font-light text-sm'>Address</p>
+                <p className='text-white font-semibold text-lg mt-1'>
+                  Ethereum
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
