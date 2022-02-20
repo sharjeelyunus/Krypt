4971170965166f1109f65eba204077907632348d
sharjeelyunus <sharjeel924@gmail.com>
2022-02-20 23:58:42 +0500

    👌 IMPROVE: Contract

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 contract Transactions {
-    uint256 transactionCounter;
+    uint256 transactionCount;
 
     event Transfer(
         address from,
@@ -24,4 +24,44 @@ contract Transactions {
     }
 
     TransferStruct[] transactions;
+
+    function addToBlockchain(
+        address payable reciever,
+        uint256 amount,
+        string memory message,
+        string memory keyword
+    ) public {
+        transactionCount += 1;
+        transactions.push(
+            TransferStruct(
+                msg.sender,
+                reciever,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
+
+        emit Transfer(
+            msg.sender,
+            reciever,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
+    }
+
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {
+        return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
+    }
 }
